cc68b13ff0021dec546738d4639b5da3f7bb1e02
Jordi M. Zambrano <nickschoey@gmail.com>
2020-02-07 17:57:08 +0100

    first view of character

--- src/Character.js
+++ src/Character.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
-import styled from 'styled-components';
-import { planets, characters } from './data';
 import { Container } from 'nes-react';
+import styled from 'styled-components';
+import state from './data';
 
 const Character = () => {
   const { id } = useParams();
-  const person = characters.data[id];
+  const person = state.characters.data[id];
+  const planet = state.planets.data[person.homeworld];
+  const vehicles = person.vehicles.map(vehicle => {
+    return state.vehicles.data[vehicle];
+  });
+  const starships = person.starships.map(starship => {
+    return state.starships.data[starship];
+  });
+  const films = person.films.map(film => {
+    return state.films.data[film];
+  });
+  console.log(vehicles);
+
   console.log(person);
   return (
     <MainContainer>
@@ -28,7 +40,7 @@ const Character = () => {
           </p>
           <p>
             <strong>Born in </strong>
-            {planets.data[person.homeworld].name}
+            {planet.name}
             <strong> in </strong>
             {person.birth_year}
           </p>
@@ -53,9 +65,24 @@ const Character = () => {
           </p>
         </Container>
       </div>
-      <div style={{ display: 'flex' }}>
-        <Container title="Vehicles"></Container>
-        <Container title="Starships"></Container>
+      <div style={{ display: 'flex', width: '950px' }}>
+        <Container title="Vehicles" style={{ flexGrow: 1 }}>
+          {vehicles.map(vehicle => (
+            <p>{vehicle.name}</p>
+          ))}
+        </Container>
+        <Container title="Starships" style={{ flexGrow: 1 }}>
+          {starships.map(starship => (
+            <p>{starship.name}</p>
+          ))}
+        </Container>
+      </div>
+      <div style={{ display: 'flex', width: '950px' }}>
+        <Container title="Films" style={{ flexGrow: 1 }}>
+          {films.map(film => (
+            <p>{film.title}</p>
+          ))}
+        </Container>
       </div>
     </MainContainer>
   );
