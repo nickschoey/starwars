d5d869693976c9e85b1bbe1711b99277eacdf797
Jordi M. Zambrano <nickschoey@gmail.com>
2020-02-08 18:05:48 +0100

    Added single planet view

--- src/components/Planets/Planet.js
+++ src/components/Planets/Planet.js
@@ -1,9 +1,88 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import styled from 'styled-components';
+import state from '../../data';
+import { Container } from 'nes-react';
+// name: "Alderaan"
+// rotation_period: "24"
+// orbital_period: "364"
+// diameter: "12500"
+// climate: "temperate"
+// gravity: "1 standard"
+// terrain: "grasslands, mountains"
+// surface_water: "40"
+// population: "2000000000"
+// residents: (3) [5, 68, 81]
+// films: (2) [6, 1]
+// created: "2014-12-10T11:35:48.479000Z"
+// edited: "2014-12-20T20:58:18.420000Z"
+
+const formatNumber = num => {
+  return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
+};
 
 const Planet = () => {
   const { id } = useParams();
-  return <div>{id}</div>;
+  const planet = state.planets.data[id];
+
+  const renderNumber = (data, measure = '', unknownMsg = 'Unknown') => {
+    const parsedNumber = parseInt(data, 10);
+    // eslint-disable-next-line no-restricted-globals
+    if (isNaN(parsedNumber)) {
+      return unknownMsg;
+    }
+    return `${formatNumber(parsedNumber)} ${measure}`;
+  };
+
+  return (
+    <MainContainer>
+      <Container style={{ flex: 1 }}>
+        <p>
+          <strong>Planet: </strong>
+          {`${planet.name} (${renderNumber(
+            planet.population,
+            'population',
+            'Population Unknown'
+          )})`}
+        </p>
+        <p>
+          <strong>Rotation Period: </strong>
+          {renderNumber(planet.rotation_period, 'hours')}
+        </p>
+        <p>
+          <strong>Orbital Period: </strong>
+          {renderNumber(planet.orbital_period, 'days')}
+        </p>
+        <p>
+          <strong>Diameter: </strong>
+          {renderNumber(planet.diameter, 'km.')}
+        </p>
+        <p>
+          <strong>Climate: </strong>
+          {`${planet.climate}`}
+        </p>
+        <p>
+          <strong>Gravity: </strong>
+          {`${planet.gravity}`}
+        </p>
+        <p>
+          <strong>Terrain: </strong>
+          {`${planet.terrain}`}
+        </p>
+        <p>
+          <strong>Surface Water: </strong>
+          {renderNumber(planet.surface_water, '%')}
+        </p>
+      </Container>
+    </MainContainer>
+  );
 };
+
+const MainContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
 
 export default Planet;
--- src/components/Planets/PlanetCard.js
+++ src/components/Planets/PlanetCard.js
@@ -2,10 +2,15 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Container } from 'nes-react';
 import styled from 'styled-components';
+import { useHistory } from 'react-router-dom';
 
 const PlanetCard = ({ planet }) => {
+  const history = useHistory();
+  const navToPlanet = () => {
+    history.push(`/planet/${planet.id}`);
+  };
   return (
-    <PlanetCardContainer>
+    <PlanetCardContainer onClick={navToPlanet}>
       <Container>{planet.name}</Container>
     </PlanetCardContainer>
   );
