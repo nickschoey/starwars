78fc1a6624b2e1c6432a01963c77e3eb4a08fa00
Jordi M. Zambrano <nickschoey@gmail.com>
2020-02-09 10:25:46 +0100

    importimage handles erros loading luke

--- src/helper/importImage.js
+++ src/helper/importImage.js
@@ -1,14 +1,14 @@
 const loadImage = (id, setImage) => {
-  if (id < 5) {
-    //  eslint-disable-line
-    import(`../assets/${id}.png`).then(image => {
+  //  eslint-disable-line
+  import(`../assets/${id}.png`)
+    .then(image => {
       setImage(image.default);
+    })
+    .catch(() => {
+      import(`../assets/1.png`).then(image => {
+        setImage(image.default);
+      });
     });
-  } else {
-    import(`../assets/1.png`).then(image => {
-      setImage(image.default);
-    });
-  }
 };
 
 export default loadImage;
