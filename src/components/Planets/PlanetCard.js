2944483dcd891d10bdcec445870e87d6ab1c816d
Jordi M. Zambrano <nickschoey@gmail.com>
2020-02-09 11:00:43 +0100

    removed unneeded dependency

--- src/components/Planets/PlanetCard.js
+++ src/components/Planets/PlanetCard.js
@@ -1,20 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Container } from 'nes-react';
-import styled from 'styled-components';
-import { useHistory } from 'react-router-dom';
 import Anchor from '../common/Anchor';
 
 const PlanetCard = ({ planet }) => {
-  const history = useHistory();
-  const navToPlanet = () => {
-    history.push(`/planet/${planet.id}`);
-  };
   return (
     <Anchor to={`/planet/${planet.id}`}>
-      <PlanetCardContainer>
-        <Container>{planet.name}</Container>
-      </PlanetCardContainer>
+      <Container>{planet.name}</Container>
     </Anchor>
   );
 };
@@ -22,11 +14,5 @@ const PlanetCard = ({ planet }) => {
 PlanetCard.propTypes = {
   planet: PropTypes.node.isRequired
 };
-
-const PlanetCardContainer = styled.div`
-  &:hover {
-    cursor: url(https://unpkg.com/nes.css/assets/cursor-click.png);
-  }
-`;
 
 export default PlanetCard;
