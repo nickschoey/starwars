17cecc72fabe8940a7cdc38054ef8c725961261e
Jordi M. Zambrano <nickschoey@gmail.com>
2020-02-09 10:25:28 +0100

    anchor takes color as prop

--- src/components/common/Anchor.js
+++ src/components/common/Anchor.js
@@ -3,22 +3,30 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
-const Anchor = ({ children, to }) => {
-  return <StyledLink to={to}>{children}</StyledLink>;
+const Anchor = ({ children, to, color }) => {
+  return (
+    <StyledLink color={color} to={to}>
+      {children}
+    </StyledLink>
+  );
 };
 
 const StyledLink = styled(Link)`
-  color: #b6231e;
+  color: ${props => props.color};
   text-decoration: none;
   &:hover {
     text-decoration: none;
-    color: #8b2338;
   }
 `;
 
 Anchor.propTypes = {
   children: PropTypes.node.isRequired,
-  to: PropTypes.string.isRequired
+  to: PropTypes.string.isRequired,
+  color: PropTypes.string
+};
+
+Anchor.defaultProps = {
+  color: '#b6231e'
 };
 
 export default Anchor;
