3b6ec6c73f78a5373baa55effa56c08fb2ae0b4a
Jordi M. Zambrano <nickschoey@gmail.com>
2020-02-12 23:56:11 +0100

    remove logger for production

--- src/index.js
+++ src/index.js
@@ -3,13 +3,12 @@ import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
-import logger from 'redux-logger';
 import * as serviceWorker from './serviceWorker';
 import App from './App';
 import reducers from './reducers';
 import './index.css';
 
-const store = createStore(reducers, applyMiddleware(thunk, logger));
+const store = createStore(reducers, applyMiddleware(thunk));
 
 ReactDOM.render(
   <Provider store={store}>
