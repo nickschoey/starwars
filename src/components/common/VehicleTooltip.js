29df80b84c55241e5a1f0764d836d3db0da2c5c9
Jordi M. Zambrano <nickschoey@gmail.com>
2020-02-12 23:55:24 +0100

    capitalize

--- src/components/common/VehicleTooltip.js
+++ src/components/common/VehicleTooltip.js
@@ -3,21 +3,25 @@ import PropTypes from 'prop-types';
 import ReactTooltip from 'react-tooltip';
 import renderNumber from '../../helper/renderNumber';
 import { colors } from '../../helper/constants';
+import capitalize from '../../helper/capitalize';
 
 const VehicleTooltip = ({ vehicle, align }) => {
   return (
     <ReactTooltip id={vehicle.url} place={align}>
       <div>{vehicle.name}</div>
       <p>{`${vehicle.manufacturer}`}</p>
-      <li>{`Class: ${vehicle.vehicle_class || vehicle.starship_class}`}</li>
+      <li>
+        {`Class: ${capitalize(vehicle.vehicle_class) ||
+          vehicle.starship_class}`}
+      </li>
       <li>
         {`Max Speed: ${renderNumber(vehicle.max_atmosphering_speed, 'kph')}`}
       </li>
       {vehicle.hyperdrive_rating && (
         <li>{`Hyperdrive Rating: ${vehicle.hyperdrive_rating}`}</li>
       )}
-      <li>{`Crew: ${vehicle.crew}`}</li>
-      <li>{`Passengers: ${vehicle.passengers}`}</li>
+      <li>{`Crew: ${renderNumber(vehicle.crew)}`}</li>
+      <li>{`Passengers: ${renderNumber(vehicle.passengers)}`}</li>
       <li>{`Cargo: ${renderNumber(vehicle.cargo_capacity, ' tons')}`}</li>
       <div style={{ padding: '8px', color: colors.starWarsYellow }}>
         {renderNumber(vehicle.cost_in_credits, ' credits', 'Unknown Cost')}
