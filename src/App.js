8dd02a8b9d03561235b674b034810a9c1dfa5b4f
Jordi Martínez Zambrano <jordi.martinez@sap.com>
2020-02-13 08:19:47 +0100

    introduced useSwapi customHook

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
 import { StickyContainer } from 'react-sticky';
 
 import Character from './components/Characters/Character';
@@ -8,54 +7,16 @@ import Hero from './components/Hero';
 import Planets from './components/Planets';
 import Films from './components/Films';
 import Characters from './components/Characters';
-
-import getCharacters, { getCharactersSuccess } from './actions/getCharacters';
-import getFilms, { getFilmsSuccess } from './actions/getFilms';
-import getStarships, { getStarshipsSuccess } from './actions/getStarships';
-import getSpecies, { getSpeciesSuccess } from './actions/getSpecies';
-import getVehicles, { getVehiclesSuccess } from './actions/getVehicles';
-import getPlanets, { getPlanetsSuccess } from './actions/getPlanets';
 import LoadModal from './components/LoadModal';
 import Planet from './components/Planets/Planet';
 import Film from './components/Films/Film';
 import Github from './components/common/Github';
 import useLoadStatus from './helper/useLoadStatus';
+import useSwapi from './helper/useSwapi';
 
 const App = () => {
-  const dispatch = useDispatch();
   const dataLoaded = useLoadStatus();
-
-  useEffect(() => {
-    const cachedCharacters = JSON.parse(localStorage.getItem('characters'));
-    const cachedFilms = JSON.parse(localStorage.getItem('films'));
-    const cachedSpecies = JSON.parse(localStorage.getItem('species'));
-    const cachedVehicles = JSON.parse(localStorage.getItem('vehicles'));
-    const cachedPlanets = JSON.parse(localStorage.getItem('planets'));
-    const cachedStarships = JSON.parse(localStorage.getItem('starships'));
-
-    if (
-      cachedCharacters &&
-      cachedFilms &&
-      cachedSpecies &&
-      cachedVehicles &&
-      cachedPlanets &&
-      cachedStarships
-    ) {
-      dispatch(getCharactersSuccess(cachedCharacters));
-      dispatch(getFilmsSuccess(cachedFilms));
-      dispatch(getSpeciesSuccess(cachedSpecies));
-      dispatch(getVehiclesSuccess(cachedVehicles));
-      dispatch(getPlanetsSuccess(cachedPlanets));
-      dispatch(getStarshipsSuccess(cachedStarships));
-    } else {
-      dispatch(getCharacters());
-      dispatch(getFilms());
-      dispatch(getStarships());
-      dispatch(getSpecies());
-      dispatch(getVehicles());
-      dispatch(getPlanets());
-    }
-  }, [dispatch]);
+  useSwapi();
 
   return (
     <Router>
