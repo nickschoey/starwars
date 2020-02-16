f361ee561bd6ef4e7f7e06bfb3b0a3069ad0dd8a
Jordi M. Zambrano <nickschoey@gmail.com>
2020-02-16 11:22:16 +0100

    testing anchor with react-testing-library

--- src/components/common/Anchor.test.js
+++ src/components/common/Anchor.test.js
@@ -1,29 +1,44 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import 'jest-styled-components';
 import Anchor from './Anchor';
-import { findByTestAttr, checkProps } from '../../../test/testUtils';
+import { colors } from '../../helper/constants';
 
-const defaultProps = { children: 'Link', to: '/' };
-/**
- * Factory function to create a ShallowWrapper for the Anchor component.
- * @function setup
- * @param {object} props - Component props specific to this setup
- * @param {object} state - Initial state for setup.
- * @returns {ShallowWrapper}
- */
-const setup = (props = {}, state = null) => {
-  const setupProps = { ...defaultProps, ...props };
-  const wrapper = shallow(<Anchor {...setupProps} />);
-  if (state) wrapper.setState(state);
-  return wrapper;
-};
+const testMessage = 'Test Message';
+const component = (
+  <MemoryRouter>
+    <Anchor to="/">{testMessage}</Anchor>
+  </MemoryRouter>
+);
 
-test('renders without error', () => {
-  const wrapper = setup();
-  const anchorComponent = findByTestAttr(wrapper, 'component-anchor');
-  expect(anchorComponent).toHaveLength(1);
+test('shows the children', () => {
+  render(component);
+  expect(screen.getByText(testMessage)).toBeInTheDocument();
 });
 
-test('does not throw warning with expected props', () => {
-  checkProps(Anchor, defaultProps);
+test('it applies default styles', () => {
+  render(component);
+  expect(screen.getByText(testMessage)).toHaveStyleRule(
+    'text-decoration',
+    'none'
+  );
+  expect(screen.getByText(testMessage)).toHaveStyleRule(
+    'color',
+    colors.starWarsYellow
+  );
+});
+
+test('it applies styles according to passed props', () => {
+  render(
+    <MemoryRouter>
+      <Anchor color={colors.starWarsWhite} to="/">
+        {testMessage}
+      </Anchor>
+    </MemoryRouter>
+  );
+  expect(screen.getByText(testMessage)).toHaveStyleRule(
+    'color',
+    colors.starWarsWhite
+  );
 });
